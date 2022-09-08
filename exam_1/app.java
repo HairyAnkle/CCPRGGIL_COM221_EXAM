aa03f227f867d4cc04c80ffc99e115034ac066ee
Cristian P. Ogena <111488747+HairyAnkle@users.noreply.github.com>
2022-09-08 14:24:44 +0800

    Create app.java

---
+++ exam_1/app.java
@@ -0,0 +1,12 @@
+public class App {
+    
+    public static void main(String[] args) {
+        
+        //Object
+
+        Student Me = new Student("Ogena", "Cristian", 'P', 12, 14, 2003, 2022104019, "ogenacp@students.national-u.edu.ph", true);
+        Me.sayMyEmailAddress();
+        Me.sayMyStudentNumber();
+        Me.amIAwesome();
+    }
+}
